0ba1f8ae571bffc062886010d200a8d87a952439
lawsumisu <sonokin@gmail.com>
2019-02-04 01:04:36 -0800

    Implemented wind stream logic to work with particles.

---
+++ Assets/ListUtilities.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ListUtilities{
+
+    /// <summary>
+    /// A Data Structure designed for maintaining a set number of elements in the order that they are added.
+    /// When the n+1th element is added to a list of size n, the "window" of accessible elements is shifted
+    /// by 1, resulting in the removal of the 1st element. This window-shift occurs in O(1) time.
+    /// This also means that (for a full array) the least recently added element is always at index
+    /// "0" and the most recently added element is always at index "size-1".
+    /// </summary>
+    public class WindowArray<T> {
+        private T[] array;
+        private int windowIndex = 0;
+        private int size;
+
+        //Constructors
+        public WindowArray(int size) {
+            this.size = size;
+            array = new T[size];
+
+        }
+        public WindowArray(T[] array) {
+            this.array = array;
+            size = array.Length;
+        }
+        //Methods
+        public void Add(T item) {
+            array[windowIndex % size] = item;
+            windowIndex++;
+
+        }
+
+        /// <summary>
+        /// Gets the element that was most recently added to this array.
+        /// </summary>
+        /// <returns>The last.</returns>
+        public T GetLast() {
+            return this[size - 1];
+        }
+        //Extra methods to make use similar to arrays.
+        public static implicit operator WindowArray<T>(T[] array) {
+            return new WindowArray<T>(array);
+        }
+        public List<T> ToList() {
+            List<T> output = new List<T>();
+            for (int i = 0; i < size; i++) {
+                output.Add(this[i]);
+            }
+            return output;
+        }
+        public T this[int i] {
+            get {
+                int ii = i % size;
+                if (windowIndex < size) {
+                    return array[(ii + size) % size];
+                }
+                else {
+                    return array[(windowIndex + ii + size) % size];
+                }
+            }
+            set {
+                int ii = i % size;
+                if (windowIndex < size) {
+                    array[(ii + size) % size] = value;
+                }
+                else {
+                    array[(windowIndex + ii + size) % size] = value;
+                }
+            }
+        }
+        public int Length {
+            get { return Mathf.Min(windowIndex, size); }
+        }
+
+        public void Fill(T item) {
+            for (int i = 0; i < size; i++) {
+                Add(item);
+            }
+        }
+    }
+
+}
--- Assets/WindStream.cs
+++ Assets/WindStream.cs
@@ -1,57 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ListUtilities;
 
 public class WindStream : MonoBehaviour {
 
-    ParticleSystem ps;
-
     // these lists are used to contain the particles which match
     // the trigger conditions each frame.
-    List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
-    ParticleSystem m_System;
-    ParticleSystem.Particle[] m_Particles;
-    SphereCollider[] colliders;
-    float radius = 1.0f;
-    float k = 10;
+    public ParticleSystem ps;
+    ParticleSystem.Particle[] particles;
+    float radius = 2.0f;
+    float k = 50;
+    public float speed = 3;
+    public float angularSpeed = 160;
+    private WindowArray<Vector3> points;
 
     void Start() {
-        ps = GetComponentInChildren<ParticleSystem>();
-        colliders = GetComponentsInChildren<SphereCollider>();
+        points = new WindowArray<Vector3>(50);
+    }
+
+    void Update() {
+        UpdateInput();
+        int N = points.Length;
+        if (N == 0 || (points[N - 1] - transform.position).magnitude > .1f) {
+            points.Add(transform.position);
+        }
+        DrawPath();
     }
 
     void LateUpdate() {
-        InitializeIfNeeded();
-        int count = ps.GetParticles(m_Particles);
+        InitializeParticleSystem();
+        int particleCount = ps.GetParticles(particles);
 
         // Change only the particles that are alive
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < particleCount; i++) {
             // Apply forces to particles when they intersect colliders
-            Vector3 q = m_Particles[i].position;
-            Vector3 F = Vector3.zero;
-            for (int j = 0; j < colliders.Length - 1; ++j) {
-                int k0 = j, k1 = j == colliders.Length - 1 ? 0 : j + 1;
-                Vector3 p1 = colliders[k0].transform.position;
-                Vector3 p2 = colliders[k1].transform.position;
-                if (IsPointInCylinder(q, p1, p2, radius)) {
-                    Vector3 a = q - p1;
-                    Vector3 b = p2 - p1;
-                    Vector3 rejection = a - Vector3.Dot(a, b) / b.sqrMagnitude * b;
-                    // Apply Forces
-                    Vector3 F1 = b;
-                    Vector3 F2 = -k * (rejection.magnitude) * rejection.normalized;
-                    F += F1 + F2;
-                    Debug.DrawLine(q, q - rejection, Color.red);
-                    Debug.DrawLine(q, p1, Color.blue);
-                    Debug.DrawLine(p2, p1, Color.yellow);       
-                }
-            }
-            m_Particles[i].velocity += F * Time.deltaTime;
-            Debug.DrawLine(q, q + m_Particles[i].velocity, Color.green);
+            Vector3 q = particles[i].position;
+            Vector3 v = particles[i].velocity;
+            float dt = Time.deltaTime / 2;
+            Vector3 F = CalculateForce(q);
+            v += F * dt;
+            q += v * dt;
+
+            F = CalculateForce(q);
+            v += F * dt;
+            particles[i].velocity = v;
+
+            Vector3 d = CalculateVelocity(q);
+            particles[i].position += d * Time.deltaTime * speed;
         }
 
         // Apply the particle changes to the Particle System
-        m_System.SetParticles(m_Particles, count);
+        ps.SetParticles(particles, particleCount);
+    }
+
+    Vector3 CalculateForce(Vector3 particlePosition) {
+        for (int j = points.Length - 1; j > 0; --j) {
+            int k0 = j - 1, k1 = j;
+            Vector3 p1 = points[k0];
+            Vector3 p2 = points[k1];
+            if (IsPointInCylinder(particlePosition, p1, p2, radius)) {
+                Vector3 a = particlePosition - p1;
+                Vector3 b = p2 - p1;
+                Vector3 rejection = a - Vector3.Dot(a, b) / b.sqrMagnitude * b;
+                // Apply Forces
+                Vector3 F1 = -k * (rejection.magnitude - .4f) * rejection.normalized;
+                Vector3 F2 = Vector3.Cross(b, rejection);
+                return F1 + F2;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    Vector3 CalculateVelocity(Vector3 particlePosition) {
+        for (int j = points.Length - 1; j > 0; --j) {
+            // Check the cylinders in reverse order
+            int k0 = j - 1, k1 = j;
+            Vector3 p1 = points[k0];
+            Vector3 p2 = points[k1];
+            if (IsPointInCylinder(particlePosition, p1, p2, radius)) {
+                // Only take the first cylinder's velocity
+                return (p2 - p1).normalized;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    void UpdateInput() {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            direction += Vector2.down;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            direction += Vector2.right;
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            direction += Vector2.left;
+        }
+        direction *= Time.deltaTime * angularSpeed;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x + direction.x, transform.eulerAngles.y + direction.y, 0);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
     bool IsPointInCylinder(Vector3 q, Vector3 p1, Vector3 p2, float r) {
@@ -60,11 +110,18 @@ public class WindStream : MonoBehaviour {
         return Vector3.Dot(q - p1, v) >= 0 && Vector3.Dot(q - p2, v) <= 0 && cross.magnitude / v.magnitude <= r;
     }
 
-    void InitializeIfNeeded() {
-        if (m_System == null)
-            m_System = GetComponentInChildren<ParticleSystem>();
+    void InitializeParticleSystem() {
+        if (ps == null)
+            ps = GetComponentInChildren<ParticleSystem>();
 
-        if (m_Particles == null || m_Particles.Length < m_System.main.maxParticles)
-            m_Particles = new ParticleSystem.Particle[m_System.main.maxParticles];
+        if (particles == null || particles.Length < ps.main.maxParticles)
+            particles = new ParticleSystem.Particle[ps.main.maxParticles];
+    }
+
+    void DrawPath() {
+        for (int i = 0; i < points.Length - 1 ; ++i) {
+            Vector3 p1 = points[i], p2 = points[i + 1];
+            Debug.DrawLine(p1, p2, Color.cyan);
+        }
     }
 }
